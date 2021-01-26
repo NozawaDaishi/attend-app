a3791747e443aa08fc9c1e1c37729f0bc608dc77
kaito.chika <kaito.chika@ZTA20096.local>
2021-01-26 14:55:55 +0900

    出席状況を表示する処理を実装

--- app/controllers/attends_controller.rb
+++ app/controllers/attends_controller.rb
@@ -1,5 +1,12 @@
 class AttendsController < ApplicationController
     def index
-        # @current_attendance = 
+        @today = Time.zone.now.strftime('%Y-%m-%d')
+        @myklass = User.myklass('R4A1')
+        # if current_user&.teacher?
+        #     @current_attendance = 
+        #     User.myklass('R4A1').each do |user|
+        #         user.attends.where("date LIKE ?", "%#{today}%")
+        #     end
+        # end
     end
 end
