3b1196850b989286d6e074fc1e5d9cfccc124286
kaito.chika <kaito.chika@ZTA20096.local>
2021-01-26 11:46:31 +0900

    seedにてfakerを使用

--- app/controllers/attends_controller.rb
+++ app/controllers/attends_controller.rb
@@ -2,6 +2,5 @@ class AttendsController < ApplicationController
     before_action :required_logged_in
 
     def index
-        
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,16 +10,33 @@ User.create!(
     role: 1
 )
 
-User.create!(
-    uid: 'uuuuuuid2',
-    last_name: '野崎',
-    first_name: 'ホビーマン',
-    klass: 'R4A1',
-    number: 171032,
-    password: 'password',
-    role: 1
-)
+## R4A1
+10.times do |i|
+    User.create!(
+        uid: 'uuid' + (i + 1).to_s,
+        last_name: Faker::Name::last_name,
+        first_name: Faker::Name::first_name,
+        klass: 'R4A1',
+        number: 171032 + i,
+        password: 'password',
+        role: 1
+    )
+end
 
+## R4A1
+10.times do |i|
+    User.create!(
+        uid: 'uuuid' + (i + 1).to_s,
+        last_name: Faker::Name::last_name,
+        first_name: Faker::Name::first_name,
+        klass: 'R4A2',
+        number: 172032 + i,
+        password: 'password',
+        role: 1
+    )
+end
+
+## 先生
 User.create!(
     uid: 'uuuuuuid3',
     last_name: '丸山',
