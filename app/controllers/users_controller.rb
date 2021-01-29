3c8b1110e71cce0c7b13feae6db4d853d86e1aff
NozawaDaishi <nozawa171040@gmail.com>
2021-01-29 12:27:53 +0900

    ユーザー登録時のバグ修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,6 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            log_in @user
             redirect_to @user, notice: "ユーザーを登録しました。"
         else
             render "new"
