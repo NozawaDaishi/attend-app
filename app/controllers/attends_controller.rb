e0af533cc6e46938100dbf5243e1bd6033c32098
Alt70155 <chika1086@gmail.com>
2021-02-01 12:05:50 +0900

    教師用ページで出席率を見れるよう変更

--- app/controllers/attends_controller.rb
+++ app/controllers/attends_controller.rb
@@ -5,20 +5,21 @@ class AttendsController < ApplicationController
         if current_user.teacher?
             @klass = params[:klass] || current_user.klass
             @students = User.students(@klass)
+            @attend_rate_list = @students.map { |user| calc_attend_rate(user) }
         else
-            @attend_rate = calc_attend_rate
+            @attend_rate = calc_attend_rate(current_user)
         end
     end
 
     private
 
-        def calc_attend_rate
+        def calc_attend_rate(user)
             # 2021年1月19日から学校が始まった想定で計算
             start_date = Date.parse('2021-01-19')
             # 学校が始まった日から昨日までの営業日数を計算
             total_business_days = start_date.business_days_until(Date.today) + 1
             # 今までの出席日数を取得
-            total_attend_days   = current_user.attends.count
+            total_attend_days   = user.attends.count
 
             (total_attend_days / total_business_days.to_f * 100).round(2)
         end
